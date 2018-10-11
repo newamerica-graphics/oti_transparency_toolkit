9b30c50e5039085f5aabf6d3d772aba2323ce442
lorenries <lorenries@users.noreply.github.com>
2018-10-11 16:00:33 -0400

    change title/source for the footnotes

--- src/components/ChartContainer/index.js
+++ src/components/ChartContainer/index.js
@@ -1,10 +1,45 @@
 import React from "react";
 import "./ChartContainer.scss";
 
-const Title = props => <h3 className="chart__title">{props.title}</h3>;
+const Title = props => (
+  <h3 className="chart__title">
+    {props.title.split("@").map((s, i) => {
+      if (i === 0) {
+        return <span>{s}</span>;
+      } else {
+        return (
+          <sup style={{ paddingLeft: "0.25rem", fontWeight: "normal" }}>
+            {s}
+          </sup>
+        );
+      }
+    })}
+  </h3>
+);
 const Subtitle = props => <h4 className="chart__subtitle">{props.subtitle}</h4>;
 const Source = props => (
-  <span className="chart__source">Source: {props.source}</span>
+  <div
+    className="chart__source"
+    style={{ paddingTop: "0.5rem", fontSize: "14px" }}
+  >
+    {Object.keys(props.source).map(key => {
+      console.log(props.source);
+      if (key.includes("source") && props.source[key]) {
+        return (
+          <div style={{ paddingBottom: "0.5rem" }}>
+            {props.source[key] &&
+              props.source[key].split("@").map((s, i) => {
+                if (i === 0) {
+                  return <sup style={{ paddingRight: "0.25rem" }}>{s}</sup>;
+                } else {
+                  return <span>{s}</span>;
+                }
+              })}
+          </div>
+        );
+      }
+    })}
+  </div>
 );
 const ChartContainer = props => (
   <div className="chart">
